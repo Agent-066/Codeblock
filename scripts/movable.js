3976c82958dfc0a3c16e41e95d9024d11e4bd8d4
Agent-066 <99182818+Agent-066@users.noreply.github.com>
2026-02-25 08:04:38 +0700

    почти доделал с сохранение
    пока что это

---
+++ scripts/movable.js
@@ -0,0 +1,41 @@
+let el = null;
+let safe = document.querySelector('.safe-zone');
+let f_D = false;
+let X = 0;
+let Y = 0;
+
+    document.addEventListener('mousedown', function(e) 
+{
+    let movable = e.target.closest(".movable");
+    if (!movable) return;
+    el = movable;
+    el.style.cursor = 'move';
+    f_D = true;
+    X = e.clientX - el.offsetLeft;
+    Y = e.clientY - el.offsetTop;
+    el.style.cursor = 'grabbing';
+    e.preventDefault();
+});
+
+document.addEventListener('mousemove', function(e) 
+{
+    if (!f_D || !el) return;
+    let Xi = e.clientX - X;
+    let Yi = e.clientY - Y;
+    Xi = Math.max(safe.offsetLeft, Math.min(safe.clientWidth + safe.offsetLeft - el.clientWidth, Xi));
+    Yi = Math.max(safe.offsetTop, Math.min(safe.clientHeight + safe.offsetTop - el.clientHeight, Yi));
+    el.style.left = Xi + 'px';
+    el.style.top = Yi + 'px';
+});
+
+document.addEventListener('mouseup', function()
+{
+    if (el) el.style.cursor = 'move';
+    f_D = false;
+});
+
+document.addEventListener('mouseleave', function()
+{
+    if (el) el.style.cursor = 'move';
+    f_D = false;
+});
